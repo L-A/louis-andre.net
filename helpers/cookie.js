39afbbc9520f475fd74b970ebd19c060b7063ac8
Louis-André Labadie <monsieur@louis-andre.net>
2017-02-24 15:49:12 -0500

    Support basic translation

--- helpers/cookie.js
+++ helpers/cookie.js
@@ -1,5 +1,11 @@
+let serverCookies = {}
+
 const writeCookie = (name, value) => {
-  document.cookie = name + "=" + value + "; path=/"
+  if (typeof document != 'undefined') {
+    document.cookie = name + "=" + value + "; path=/"
+  } else {
+    serverCookies[name] = value;
+  }
 }
 
 const readCookie = (name, req = false) => {
@@ -8,19 +14,24 @@ const readCookie = (name, req = false) => {
 
   if (req) {
     cookieString = req.headers.cookie
-  } else {
+  } else if (typeof document != 'undefined') {
     cookieString = document.cookie
+  } else if (serverCookies[name] != undefined) {
+    return serverCookies[name]
   }
 
-  if (cookieString == undefined) { return null } 
+  if (cookieString == undefined) { return null }
 
   let cookieArray = cookieString.split(';')
 
   for (var i = 0; i < cookieArray.length; i++) {
     let cookie = cookieArray[i]
     while (cookie.charAt(0) == " ") cookie = cookie.substring (1)
-    if ( cookie.indexOf(namePrefix) == 0 ) return cookie.substring (namePrefix.length)
+    if ( cookie.indexOf(namePrefix) == 0 ) {
+      return cookie.substring (namePrefix.length)
+    }
 	}
+
   return null
 }
 
---
+++ helpers/lang.js
@@ -0,0 +1,30 @@
+import { readCookie, writeCookie } from '~/helpers/cookie'
+
+const Translate = (locales) => {
+  const Key = (path) => {
+    let loc = locales[Language()]
+    let p
+    for (let i=0, p=path.split('.'), len=p.length; i<len; i++){
+     loc = loc[p[i]]
+    }
+    return loc
+  }
+  return { Key }
+}
+
+const Language = () => {
+  let langCookie = readCookie("lang")
+
+  if (langCookie != null) {
+    return langCookie
+  } else {
+    return "en"
+  }
+
+}
+
+const SetLanguage = (lang) => {
+  writeCookie("lang", lang)
+}
+
+export { Translate, SetLanguage, Language }
