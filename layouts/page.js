92441b3b29cd94f85bf75eeb73bcd5de3d27ec49
Louis-André Labadie <monsieur@louis-andre.net>
2017-03-01 10:28:19 -0500

    Add Cushion availability to page data & Contact page

--- layouts/page.js
+++ layouts/page.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Head from '~/components/head' // I really should change this nomenclature
 import Header from '~/components/header'
 import Footer from '~/components/footer'
+import 'isomorphic-fetch'
 import { writeCookie, readCookie } from '~/helpers/cookie'
+
+const cushionURL = 'https://my.cushionapp.com/api/v1/users/745f2179-6958-4664-8549-dce939fb32e6/availability'
+let availableDate = false
 
 const defaultOpts = {
   naked: false,
@@ -13,15 +17,26 @@ export default (WrappedComponent, opts) => {
   opts = {...defaultOpts, ...opts}
 
   return class extends React.Component {
-
     static async getInitialProps (ctx) {
+
+      // Language & lang cookie
       let lang = readCookie("lang", ctx.req)
       let pageProps = {}
+
       writeCookie("lang", lang)
       if (WrappedComponent.getInitialProps) {
         pageProps = await WrappedComponent.getInitialProps(ctx);
       }
-      return {...WrappedComponent.props, ...pageProps, lang: lang}
+
+      // Cushion Availability (the answer is saved as the month, 0-11)
+      if (availableDate == false) {
+        console.log("Need to fetch Cushion")
+        let cushionAvailability = await fetch(cushionURL)
+        let availabilityData = await cushionAvailability.json()
+        availableDate = await new Date(availabilityData.availability.available_on).getMonth()
+      }
+
+      return {...WrappedComponent.props, ...pageProps, lang: lang, availableMonth: availableDate}
     }
 
     render (props) {
--- pages/contact.js
+++ pages/contact.js
@@ -5,37 +5,53 @@ import { Translate } from '~/helpers/lang'
 const T = Translate({
 	en: {
 		title: "Contact & availability",
+		contracts_start: "New contracts can start in",
 		i_answer: "I answer quickly at",
-		elsewhere: "Elsewhere"
+		elsewhere: "Elsewhere",
+		contacts: {
+			dribbble: "<strong>Dribbble</strong> contact form",
+			linkedin: "<strong>Linkedin</strong> message",
+			twitter: "<strong>Twitter</strong>",
+		},
+		month: [
+			"January", "February", "March", "April",
+			"May", "June", "July", "August", "September",
+			"October", "November", "December"
+		]
 	},
 	fr: {
 		title: "Contact & disponibilité",
+		contracts_start: "Disponible à partir de",
 		i_answer: "Je réponds rapidement à",
-		elsewhere: "Ailleurs"
+		elsewhere: "Ailleurs",
+		contacts: {
+			dribbble: "Demande de travail <strong>Dribbble</strong>",
+			linkedin: "Messagerie <strong>LinkedIn</strong>",
+			twitter: "<strong>Twitter</strong>",
+		},
+		month: [
+			"janvier", "février", "mars", "avril",
+			"mai", "juin", "juillet", "août", "septembre",
+			"octobre", "novembre", "décembre"
+		]
 	}
 })
 
-export default Page( () => {
+export default Page( (props) => {
 	return (
 			<div className="ph3 mw6-l ph5-m ph0-l center">
-				<h2 className="f2 fw3 navy no-underline dim pa0">{T.Key("title")}</h2>
-				<p className="availability br2 pv2 ph3 ph4-ns green dib">
-					New contracts can start in <strong>March</strong>
-				</p>
+				<h2 className="f2 fw3 navy no-underline pa0">{T.Key("title")}</h2>
+				<CushionScript availableMonth={props.availableMonth}/>
 				<p className="email">
 					{T.Key("i_answer")} <a className="link pointer" href="mailto:monsieur@louis-andre.net">monsieur&#64;louis-andre.net</a>
 				</p>
-				<h3 className="silver bold f6 ttu mt4">Elsewhere</h3>
-				<a className="db link dribbble mb2" href="https://dribbble.com/messages/new?recipient_id=l-a">
-					<strong>Dribbble</strong> contact form
-				</a>
-				<a className="db link linkedin mb2" href="https://dribbble.com/messages/new?recipient_id=l-a">
-					<strong>Linkedin</strong> message
-				</a>
-				<a className="db link twitter mb2" href="https://twitter.com/Lalabadie">
-					<strong>Twitter</strong>
-				</a>
-
+				<h3 className="silver bold f6 ttu mt4">{T.Key("elsewhere")}</h3>
+				<a className="db link dribbble mb2" href="https://dribbble.com/l-a"
+					dangerouslySetInnerHTML={{__html: T.Key("contacts.dribbble")}} />
+				<a className="db link linkedin mb2" href="https://dribbble.com/messages/new?recipient_id=l-a"
+					dangerouslySetInnerHTML={{__html: T.Key("contacts.linkedin")}} />
+				<a className="db link twitter mb2" href="https://twitter.com/Lalabadie"
+					dangerouslySetInnerHTML={{__html: T.Key("contacts.twitter")}} />
 				<style jsx>
 					{`
 						.availability {
@@ -66,3 +82,23 @@ export default Page( () => {
 			</div>
 			)}
 , {title: T.Key("title")})
+
+const CushionScript = (props) => (
+	<p className="availability br2 pv2 ph3 ph4-ns green dib">
+		{T.Key("contracts_start") + " "}
+		<strong>
+			{T.Key("month." + (props.availableMonth))}
+		</strong>
+
+		<style jsx>
+			{`
+				.availability {
+					background-color: #ddf5db;
+					color: #08a200;
+					font-size: 14px;
+					line-height: 1.4;
+				}
+			`}
+		</style>
+	</p>
+)
