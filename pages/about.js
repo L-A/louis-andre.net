fafcde4d0e2d043a9ee8d664bdcfabf89185e440
Louis-André Labadie <monsieur@louis-andre.net>
2017-03-01 11:06:05 -0500

    Improve and translate the About page

--- pages/about.js
+++ pages/about.js
@@ -5,11 +5,19 @@ import { Translate } from '~/helpers/lang'
 const T = Translate({
   en: {
     title: "About",
-    tagline: "I design and build web sites and apps."
+    tagline: "Interdisciplinary designer",
+    p_intro: "My work involves turning broad ideas into nicely defined concepts, and then making them come to life.",
+    p_education: "<em>Some</em> years ago, I walked out of University with a formal education in graphic design. I quickly lost touch with my favorite printers and Pantone charts as I moved more and more into design for screens: web sites, web and mobiles applications.",
+    p_work: 'I have worked and learned at <a className="link dim blue" href="https://ock.am">Ockam</a>, <a className="link dim blue" href="https://mirego.com">Mirego</a>, and <a className="link dim blue" href="https://hooktstudios.com">Hookt Studios</a>. Working with some of the finest in the industry there, I made a lot of great apps for all kinds of people and devices.',
+    p_tools: 'I use tools like Photoshop, Illustrator, Sketch, Vim, a terminal, Javascript, SASS, various frameworks, libraries and other development thingamajigs as needs arise.'
   },
   fr: {
     title: "À propos",
-    tagline: "Je conçois des sites et applications web."
+    tagline: "Designer multidisciplinaire",
+    p_intro: "Mon travail consiste à prendre les idées larges et en faire des concepts bien définis, que je m’applique ensuite à réaliser.",
+    p_education: "Il y a <em>quelques</em> années, je suis sorti de l’Université avec un Bac en design graphique. J’ai rapidement perdu contact avec mes chartes Pantones et mes imprimeurs préférés, me spécialisant tranquillement en design pour les écrans: sites web, applications web et mobiles.",
+    p_work: 'J’ai appris et travaillé chez <a className="link dim blue" href="https://ock.am">Ockam</a>, <a className="link dim blue" href="https://mirego.com">Mirego</a>, et <a className="link dim blue" href="https://hooktstudios.com">Hookt Studios</a>. Accompagné des <em>tops</em> de l’industrie, j’y ai créé une multitude d’applications géniales, pour toutes sortes de gens et d’appareils.',
+    p_tools: 'J’utilise des outils comme Photoshop, Illustrator, Sketch, Vim, un terminal, Javascript, SASS, différents <em>frameworks</em> et autres bidules de programmation selon les besoins du moment.'
   }
 })
 
@@ -17,12 +25,14 @@ export default Page( () => (
 		<div className="ph3 ph5-m ph6-l relative-l">
 			<h1 className="mv4 f2 fw3 navy no-underline">{T.Key("tagline")}</h1>
 			<img className="fr mw4 mw5-ns pb3 pl3" style={imgStyle} src="static/images/about-page/portrait.png"/>
-			<p className="measure lh-copy">My work involves turning broad ideas into nicely defined concepts, and then making them come to life.</p>
-			<p className="measure lh-copy"><em>Some</em> years ago, I walked out of University with a formal education in graphic design. I quickly lost touch with my favorite printers and Pantone charts as I moved more and more into design for screens.</p>
-			<p className="measure lh-copy">I have worked and learned at <a className="link dim blue" href="http://ock.am">Ockam</a>, <a className="link dim blue" href="http://mirego.com">Mirego</a>, and <a className="link dim blue" href="http://hooktstudios.com">Hookt Studios</a>. Working with some of the finest in the industry there, I made a lot of great apps for all kinds of people and devices.</p>
-			<p className="measure lh-copy">I use tools like Photoshop, Illustrator, Sketch, Vim, a terminal, Javascript, SASS, various frameworks, libraries and other development thingamajigs as needs arise.</p>
+			<p className="measure lh-copy">{T.Key("p_intro")}</p>
+			<p className="measure lh-copy" dangerouslySetInnerHTML={{__html: T.Key("p_education")}} />
+      <p className="measure lh-copy" dangerouslySetInnerHTML={{__html: T.Key("p_work")}} />
+      <p className="measure lh-copy" dangerouslySetInnerHTML={{__html: T.Key("p_tools")}} />
 		</div>
 ), {title: T.Key("title")})
+
+
 
 const imgStyle = {
 	width: "50%"
