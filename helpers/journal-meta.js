625789e2057c87690ff8a309ae35b00a04822f4c
Louis-André Labadie <monsieur@louis-andre.net>
2020-02-12 17:02:23 -0500

    Fix journal posts' meta parsing

--- helpers/journal-meta.js
+++ helpers/journal-meta.js
@@ -1,8 +1,8 @@
 const filesMeta = preval`
-	const META = \/const\\s+meta\\s+=\\s+({[\\s\\S]*?\\n})\/
-	const SLUG = \/([^\\\/]*)(?:\\.mdx)\/
-	module.exports = require("glob").sync("./pages/journal/*.mdx").map(
-		file => {
+module.exports = require("glob").sync("./pages/journal/*.mdx").map(
+	file => {
+			const META = \/const\\s+meta\\s+=\\s+({[\\s\\S]*?\\n})\/
+			const SLUG = \/([^\\\/]*)(?:\\.mdx)\/
 			const pageSlug = SLUG.exec(file)[1]
 			const fileData = require("fs").readFileSync(file, "utf-8");
 			const match = META.exec(fileData);
