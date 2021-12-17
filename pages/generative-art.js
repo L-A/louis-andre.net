41aee0a8c0c9aef9ef94f87146b4f4234f5dc769
Louis-André Labadie <monsieur@louis-andre.net>
2021-12-17 16:33:13 -0500

    Add capacity for platform links

--- pages/generative-art.js
+++ pages/generative-art.js
@@ -8,7 +8,61 @@ import Series from "../content/art-series.json";
 
 // Image ratio is width/height
 
-const seriesViewer = ({ name, description, iterations }, index) => {
+const seriesImage = ({ name, ratio, css, ...img }, i) => {
+	// css and img are provided by plaiceholder
+	return (
+		<li className="series-iteration" key={i}>
+			<div className="image-wrapper" style={{ ...css }}>
+				<Image {...img} className="image" width={300 * ratio} height={300} />
+			</div>
+			<small>{name || "Untitled iteration"}</small>
+			<style jsx>{`
+				small {
+					opacity: 0.6;
+					font-family: "Courier New", Courier, monospace;
+				}
+
+				.series-iteration {
+					transition: transform 0.2s ease-out;
+					min-width: 332px;
+					padding-right: 32px;
+					flex: 1 0 332px;
+				}
+
+				.series-iteration::after {
+					flex: 0 0 20px;
+					content: "";
+				}
+
+				.series-iteration:last-child {
+					margin-right: 0;
+				}
+
+				.series-iteration .image-wrapper {
+					background-color: #555;
+					display: block;
+					height: 300px;
+					min-width: 300px;
+					max-height: 450px;
+					transition: transform 0.2s ease-out;
+					transform-origin: center bottom;
+					display: block;
+					width: 0;
+					max-width: 100%;
+					position: relative;
+					overflow: hidden;
+					border-radius: 2px;
+
+					box-shadow: 0px 16px 16px rgba(0, 0, 0, 0.05),
+						0px 6px 8px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.1);
+					transition: transform 0.2s ease-out, box-shadow 0.2s ease-out;
+				}
+			`}</style>
+		</li>
+	);
+};
+
+const seriesViewer = ({ name, description, platforms, iterations }, index) => {
 	useEffect(() => {
 		// Using JS variables prevents flickering in the animation
 		// useState would introduce jitter
@@ -54,23 +108,51 @@ const seriesViewer = ({ name, description, iterations }, index) => {
 		<li className="series-preview" key={name}>
 			<h2>{name}</h2>
 			<p className="description">{description}</p>
+			{platforms && (
+				<small>
+					<p className="platforms">
+						Find the collection:
+						{platforms.map(({ platform, link }) => {
+							return (
+								<>
+									{platform == "Foundation" && (
+										<a href={link} className="platform-link" key={"fnd"}>
+											<Image
+												src="/images/fnd-logo.svg"
+												width={42}
+												height={14}
+												alt={`${name} on Foundation`}
+											/>
+										</a>
+									)}
+									{platform == "Artblocks" && (
+										<a href={link} className="platform-link" key={"ab"}>
+											<Image
+												src="/images/ab-logo@3x.png"
+												width={23}
+												height={20}
+												alt={`${name} on Artblocks`}
+											/>
+										</a>
+									)}
+									{platform == "OpenSea" && (
+										<a href={link} className="platform-link" key={"os"}>
+											<Image
+												src="/images/os-logo.svg"
+												width={22}
+												height={22}
+												alt={`${name} on OpenSea`}
+											/>
+										</a>
+									)}
+								</>
+							);
+						})}
+					</p>
+				</small>
+			)}
 			<ul className={`iterations iterations-${index}`}>
-				{iterations.map(({ name, ratio, css, ...img }, i) => {
-					// css and img are provided by plaiceholder
-					return (
-						<li className="series-iteration" key={i}>
-							<div className="image-wrapper" style={{ ...css }}>
-								<Image
-									{...img}
-									className="image"
-									width={300 * ratio}
-									height={300}
-								/>
-							</div>
-							<small>{name || "Untitled iteration"}</small>
-						</li>
-					);
-				})}
+				{iterations.map(seriesImage)}
 			</ul>
 
 			<style jsx>{`
@@ -85,6 +167,11 @@ const seriesViewer = ({ name, description, iterations }, index) => {
 
 				p.description {
 					margin-top: 8px;
+				}
+
+				small {
+					color: ${Palette.text}88;
+					font-family: "Courier New", Courier, monospace;
 				}
 
 				.iterations {
@@ -114,51 +201,21 @@ const seriesViewer = ({ name, description, iterations }, index) => {
 					scroll-behavior: auto;
 				}
 
-				.series-iteration {
-					transition: transform 0.2s ease-out;
-					min-width: 332px;
-					padding-right: 32px;
-					flex: 1 0 332px;
-				}
-
-				.series-iteration::after {
-					flex: 0 0 20px;
-					content: "";
-				}
-
-				.series-iteration:last-child {
-					margin-right: 0;
-				}
-
-				.series-iteration .image-wrapper {
-					background-color: #555;
-					display: block;
-					height: 300px;
-					min-width: 300px;
-					max-height: 450px;
-					transition: transform 0.2s ease-out;
-					transform-origin: center bottom;
-					display: block;
-					width: 0;
-					max-width: 100%;
-					position: relative;
-					overflow: hidden;
-					border-radius: 2px;
-
-					box-shadow: 0px 16px 16px rgba(0, 0, 0, 0.05),
-						0px 6px 8px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.1);
-					transition: transform 0.2s ease-out, box-shadow 0.2s ease-out;
-				}
-
-				.iterations.active .image-wrapper {
+				.iterations.active :global(.image-wrapper) {
 					box-shadow: 0px 20px 20px rgba(0, 0, 0, 0.05),
 						0px 8px 16px rgba(0, 0, 0, 0.05), 0px 3px 8px rgba(0, 0, 0, 0.01);
 					transform: translateY(-1px);
 				}
 
-				small {
+				.platform-link {
+					vertical-align: middle;
+					margin-left: 8px;
+					opacity: 1;
+					transition: opacity 0.8s ease-out;
+				}
+
+				.platform-link:hover {
 					opacity: 0.6;
-					font-family: "Courier New", Courier, monospace;
 				}
 
 				@media (max-width: 960px) {
@@ -205,8 +262,8 @@ const GenerativeArt = ({ series }) => (
 	<Layout pageTitle="Generative Art">
 		<h1>Generative Art</h1>
 		<p>
-			What do you do when you enjoy coding, but want an expressive outlet where
-			the computer is a playful tool?{" "}
+			For the last few years, I've had an artistic practice based on making the
+			computer want a playful tool?{" "}
 			<Link
 				color={Palette.reading}
 				href="https://en.wikipedia.org/wiki/Generative_art"
@@ -216,6 +273,12 @@ const GenerativeArt = ({ series }) => (
 			is where it’s at! Here are some selected series.
 		</p>
 
+		<p className="notice">
+			If you want to stay up to date on my art, I post{" "}
+			<Link href="https://www.instagram.com/lalabadie/">here on Instagram</Link>
+			.
+		</p>
+
 		<p>
 			<strong>A note about NFTs:</strong> You can find my legitimate addresses
 			on Tezos and Ethereum{" "}
@@ -223,12 +286,6 @@ const GenerativeArt = ({ series }) => (
 				on this page
 			</Link>
 			, as well as my simple policy on blockchain energy use.
-		</p>
-
-		<p className="notice">
-			If you want to stay up to date on my art, I post{" "}
-			<Link href="https://www.instagram.com/lalabadie/">here on Instagram</Link>
-			.
 		</p>
 
 		<ul className="series">{series.map(seriesViewer)}</ul>
