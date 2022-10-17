e515d70dd93c7ac184ec35f90b45bcbb1a8c1bbc
Louis-André Labadie <monsieur@louis-andre.net>
2022-10-17 11:29:52 -0400

    Update blockchain information

--- pages/nft.js
+++ pages/nft.js
@@ -6,22 +6,43 @@ const NFT = () => {
 	return (
 		<Layout pageTitle="About NFTs">
 			<h1>About NFTs</h1>
-			<h2>Legitimate addresses</h2>
+			<h2>Blockchain addresses</h2>
 			<p>
-				I mint NFTs from two addresses. The first one is{" "}
-				<Link href="https://foundation.app/@lalabadie">on Ethereum</Link> (see
-				the part about carbon), from the address{" "}
+				I mint from two addresses belonging to{" "}
+				<Link href="https://petiterevolte.com/en">Petite Révolte</Link>, my art
+				and design studio. The first one is{" "}
+				<Link href="https://etherscan.io/address/0x4013a99b2761aae44031Da0Cc5510CcdEBDFa71e">
+					on Ethereum
+				</Link>{" "}
+				(see the part about carbon), from the address{" "}
+				<span
+					className="address"
+					title="0x4013a99b2761aae44031Da0Cc5510CcdEBDFa71e"
+				>
+					0x40...a71e
+				</span>
+				. The second one is{" "}
+				<Link href="https://tzkt.io/tz1TXPoPrWjn4PSbXe6L66Kqo9NVCWiEw17Q">
+					on Tezos
+				</Link>
+				, at{" "}
+				<span className="address" title="tz1TXPoPrWjn4PSbXe6L66Kqo9NVCWiEw17Q">
+					tz1T...Ew17Q
+				</span>
+				.
+			</p>
+			<p>
+				I have personal addresses which should not see much use in the future,
+				namely{" "}
 				<span
 					className="address"
 					title="0x9E7101C7741b919A389Fc9495D85843fEcAfeA50"
 				>
 					0x9E71...eA50
-				</span>
-				. The second one is{" "}
-				<Link href="https://hicetnunc.art/Louis-Andre%20Labadie">on Tezos</Link>
-				, at{" "}
+				</span>{" "}
+				and{" "}
 				<span className="address" title="tz1ZvTGYH5EBwgmaYngELeUw1qzgQRJLKoDM">
-					tz1Zv...LKoDM
+					tz1Z...KoDM
 				</span>
 				.
 			</p>
@@ -36,30 +57,32 @@ const NFT = () => {
 						<th>Offsets puchased</th>
 					</tr>
 					<tr>
-						<td>January 2021</td>
-						<td>10.31t</td>
+						<td>August 2022</td>
+						<td>11.31t</td>
 						<td>30t</td>
 					</tr>
 				</tbody>
 			</table>
 
 			<p>
-				At the time of writing this, the Ethereum blockchain is still using
-				proof of work to confirm transaction blocks. Since proof of work rewards
-				using as much energy as possible, I'll keep a running tally of
-				transactions done with my pieces, and periodically purchase carbon
-				offsets to a multiple of their estimated kgCO2 impact. This is not a
-				perfect solution, but I see it as the minimum I'm willing to live with.
+				The Ethereum blockchain has <em>finally, after all</em>, started using
+				proof of stake, an energy-efficient confirmation protocol. Its former
+				mode of operation was proof of work. Since proof of work rewards using
+				as much energy as possible, I kept a running tally of my (overall
+				modest) transactions on ETH, and periodically purchased carbon offsets
+				to a multiple of their estimated kgCO2 impact. This was not a perfect
+				solution, but it was the minimum I was willing to live with.
 			</p>
 
 			<p>
-				The per-transaction estimate is based on Kyle McDonald's{" "}
+				The per-transaction estimate was based on Kyle McDonald's{" "}
 				<Link href="https://github.com/kylemcdonald/ethereum-nft-activity">
 					ethereum NFT estimates here
 				</Link>{" "}
 				– which, for example, place Foundation at roughly 84 kgCO<sub>2</sub>{" "}
-				per transaction. At the time of writing this, most of my activity was
-				through Foundation sales, then a single Artblocks release.
+				per transaction. At the time the proof of stake merge finally happened,
+				most of my activity had been through Foundation sales (2021), then a
+				single Artblocks release (early 2022).
 			</p>
 			<p>
 				The offsets are by{" "}
