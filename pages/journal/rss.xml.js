a41bb0627c1f048cc1ed6aef9cb9fba7b8fee73b
Louis-André Labadie <monsieur@louis-andre.net>
2025-06-28 21:44:25 -0400

    Update RSS: Full content in posts

--- pages/journal/rss.xml.js
+++ pages/journal/rss.xml.js
@@ -1,21 +1,35 @@
 import path from "path";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
+import remarkGfm from "remark-gfm";
 import { getMarkdownFiles } from "../../helpers/getMarkdownFiles";
 
-const generateRSSItem = (post) => `
-  <item>
-    <guid>https://louis-andre.net/journal/${post.slug}</guid>
-    <title><![CDATA[${post.title}]]></title>
-    <link>https://louis-andre.net/journal/${post.slug}</link>
-    <description><![CDATA[${post.description || ""}]]></description>
-    <pubDate>${new Date(post.date).toUTCString()}</pubDate>
-  </item>
-`;
+const generateRSSItem = async (post) => {
+	const processedContent = await remark()
+		.use(remarkGfm) // Use the same GitHub Flavored Markdown as your main site
+		.use(remarkHtml)
+		.process(post.body || "");
 
-const generateRSS = (posts) => `<?xml version="1.0"?>
+	return `
+    <item>
+      <guid>https://louis-andre.net/journal/${post.slug}</guid>
+      <title><![CDATA[${post.title}]]></title>
+      <link>https://louis-andre.net/journal/${post.slug}</link>
+      <description><![CDATA[${post.description || ""}]]></description>
+      <content:encoded><![CDATA[${processedContent.toString()}]]></content:encoded>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+    </item>
+  `;
+};
+
+const generateRSS = async (posts) => {
+	const items = await Promise.all(posts.map(generateRSSItem));
+
+	return `<?xml version="1.0"?>
 <?xml-stylesheet type="text/xsl" href="/rss-style.xsl"?>
-  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
     <channel>
-      <title>Louis-André's journal</title>
+      <title>Louis-André Labadie</title>
       <link>https://louis-andre.net</link>
       <description>Posts on art, technology, and design</description>
       <language>en-US</language>
@@ -24,10 +38,11 @@ const generateRSS = (posts) => `<?xml version="1.0"?>
       <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
       <atom:link href="https://louis-andre.net/journal/rss.xml" rel="self" type="application/rss+xml"/>
 
-      ${posts.map(generateRSSItem).join("")}
+      ${items.join("")}
     </channel>
   </rss>
 `;
+};
 
 export default function RSS() {
 	// This should never be called since we're using getServerSideProps
@@ -40,7 +55,7 @@ export async function getServerSideProps({ res }) {
 		.sort((a, b) => new Date(b.date) - new Date(a.date))
 		.slice(0, 20); // Limit to most recent 20 posts
 
-	const rss = generateRSS(posts);
+	const rss = await generateRSS(posts);
 
 	res.setHeader("Content-Type", "text/xml");
 	res.setHeader(
